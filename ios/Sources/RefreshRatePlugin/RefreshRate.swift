8004fbabfc855267cd0a01dcb49e9ddab9885889
Charlie Benjafield <charlie.benjafield@omlet.co.uk>
2025-07-23 16:03:47 +0100

    Ignore iOS < 15

--- ios/Sources/RefreshRatePlugin/RefreshRate.swift
+++ ios/Sources/RefreshRatePlugin/RefreshRate.swift
@@ -3,29 +3,23 @@ import QuartzCore
 import WebKit
 
 @objc public class RefreshRate: NSObject {
-    private var originalRange: CAFrameRateRange? = nil
-
     @objc public func setRefreshRate(hz: Double, webView: WKWebView?) {
-        DispatchQueue.main.async {
-            if #available(iOS 15.0, *), let webView = webView {
-                if self.originalRange == nil {
-                    self.originalRange = webView.preferredFrameRateRange
-                }
-                webView.preferredFrameRateRange = CAFrameRateRange(
-                    minimum: Float(hz),
-                    maximum: Float(hz),
-                    preferred: Float(hz)
-                )
-            }
-        }
+        guard #available(iOS 15.0, *), let webView = webView else { return }
+
+        webView.preferredFrameRateRange = CAFrameRateRange(
+            minimum: Float(hz),
+            maximum: Float(hz),
+            preferred: Float(hz)
+        )
     }
 
     @objc public func resetRefreshRate(webView: WKWebView?) {
-        DispatchQueue.main.async {
-            if #available(iOS 15.0, *), let webView = webView, let original = self.originalRange {
-                webView.preferredFrameRateRange = original
-                self.originalRange = nil
-            }
-        }
+        guard #available(iOS 15.0, *), let webView = webView else { return }
+
+        webView.preferredFrameRateRange = CAFrameRateRange(
+            minimum: 30,
+            maximum: 120,
+            preferred: 120
+        )
     }
 }
